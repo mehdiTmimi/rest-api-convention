b76e792cd333cad8925aa0bb40635c968bd6fd62
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2022-12-23 10:25:25 +0100

    ok

--- app.js
+++ app.js
@@ -2,25 +2,6 @@ const express = require('express')
 const userRouter = require('./routes/users')
 const noteRouter=require('./routes/notes')
 //const bodyParser = require('body-parser');
-
-
-let notes=[
-    {
-        id:1,
-        value:20,
-        idUser:1
-    },
-    {
-        id:2,
-        value:15,
-        idUser:1
-    },
-    {
-        id:3,
-        value:19,
-        idUser:2
-    }
-]
 const app=express(); // app is a convention
 //app.use(bodyParser.urlencoded({ extended: false }));// middleware pour lire query paramss
 app.use(express.json())
